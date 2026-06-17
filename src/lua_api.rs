915c617fb2b07b569b0a4f06b0223f2f49909d27
Morier <cookingoils3@gmail.com>
2026-06-17 09:30:01 +0700

    finish most of the primitives. onto file handling

--- src/lua_api.rs
+++ src/lua_api.rs
@@ -26,12 +26,40 @@ macro_rules! register_primitives {
 	}
 }
 
-fn register_primitives(lua: &Lua, table: &mlua::Table) -> mlua::Result<()>
-{
-	register_primitives!(lua, table,
-		[
-			("move_cursor", lua_move_cursor),
-		]);
+fn register_primitives(lua: &Lua, table: &mlua::Table) -> mlua::Result<()> {
+    register_primitives!(lua, table, [
+        ("move_cursor",             lua_move_cursor),
+        ("move_cursor_to",          lua_move_cursor_to),
+        ("quit_editor",             lua_quit_editor),
+        ("insert_char",             lua_insert_char),
+        ("set_mode",                lua_set_mode),
+        ("save_mode",               lua_save_mode),
+        ("get_current_mode",                lua_get_current_mode),
+        ("get_saved_mode",          lua_get_saved_mode),
+        ("restore_mode",            lua_restore_mode),
+        ("is_minor_mode",           lua_is_minor_mode),
+        ("insert_newlines",         lua_insert_newline),
+        ("delete_after",     lua_delete_after),
+        ("delete_before",    lua_delete_before),
+        ("call_mode_hook",          lua_call_mode_hook),
+        ("set_anchor",              lua_set_anchor),
+        ("clear_anchor",            lua_clear_anchor),
+        ("delete_selected",         lua_delete_selected),
+        //TODO: ("load_config",             lua_reload_config),
+        //TODO: ("save_file",               lua_save_file),
+        ("get_line_end",            lua_get_line_end),
+        ("get_cursor_pos",          lua_get_cursor_pos),
+        ("forward_match",           lua_forward_match),
+        ("backward_match",          lua_backward_match),
+        ("forward_match_set",       lua_forward_match_set),
+        ("forward_match_notset",    lua_forward_match_notset),
+        ("backward_match_set",      lua_backward_match_set),
+        ("backward_match_notset",   lua_backward_match_notset),
+        ("char_at",                 lua_char_at),
+        ("get_total_lines",         lua_get_max_line_index),
+        //TODO: ("set_config_dir",          lua_set_config_dir),
+        //TODO: ("get_config_dir",          lua_get_config_dir),
+    ]);
 
-	return Ok(());
+    return Ok(());
 }
--- src/rust_to_lua.rs
+++ src/rust_to_lua.rs
@@ -3,7 +3,7 @@ mod helpers
 	use crate::{editor::Editor, gap_buffer::{Direction, Position}};
 	use mlua::Lua;
 	
-	pub fn get_editor(lua: &Lua) -> mlua::Result<&mut Editor>
+	pub unsafe fn get_editor(lua: &Lua) -> mlua::Result<&mut Editor>
 	{
 		let tmp = lua.app_data_ref::<*mut Editor>();
 		
@@ -32,7 +32,7 @@ mod helpers
 		forward: bool, want_in_set: bool)
 	-> mlua::Result<mlua::MultiValue>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		let cur_pos = editor.buffer.cursor_pos();
 		
 		let from_x = from_x.unwrap_or(cur_pos.x);
@@ -71,7 +71,11 @@ mod helpers
 			}
 			
 			if (forward) {i+=1;}
-			else {i-=1;}
+			else
+			{
+				if (i == 0) {break;}
+				i-=1;
+			}
 		}
 		
 		let result = [mlua::Value::Nil];
@@ -88,7 +92,7 @@ pub mod api
 	
 	pub fn lua_move_cursor(lua: &Lua, (dir, times): (String, usize)) -> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		let direction = direction_from_str(&dir)?;
 		editor.buffer.move_gap(times, direction);
 		
@@ -98,9 +102,8 @@ pub mod api
 	pub fn lua_move_cursor_to(lua: &Lua, (x, y): (usize, usize))
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
-		let line_start = editor.buffer.get_line_start(y);
-		let abs_pos = line_start + x;
+		let editor = unsafe {get_editor(lua)?};
+		let abs_pos = editor.buffer.repos_to_abspos(Position { x, y });
 		editor.buffer.move_gap_to(abs_pos);
 		
 		return Ok(());
@@ -109,7 +112,7 @@ pub mod api
 	pub fn lua_quit_editor(lua: &Lua, _: ())
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		editor.quit();
 		return Ok(());
 	}
@@ -117,7 +120,7 @@ pub mod api
 	pub fn lua_insert_char(lua: &Lua, ch: u8) 
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		editor.buffer.insert(ch);
 		return Ok(());
 	}
@@ -125,7 +128,7 @@ pub mod api
 	pub fn lua_set_mode(lua: &Lua, mode_name: String)
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		editor.set_mode(&mode_name);
 		return Ok(());
 	}
@@ -133,7 +136,7 @@ pub mod api
 	pub fn lua_insert_newline(lua: &Lua, times: usize)
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		for _ in 0..times
 		{editor.buffer.insert(b'\n');}
@@ -144,7 +147,7 @@ pub mod api
 	pub fn lua_save_mode(lua: &Lua, mode_name: String)
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		editor.save_mode(&mode_name);
 		
@@ -154,7 +157,7 @@ pub mod api
 	pub fn lua_is_minor_mode(lua: &Lua, mode_name: String)
 	-> mlua::Result<bool>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		let result = editor.is_minor_mode(&mode_name);
 		
@@ -164,7 +167,7 @@ pub mod api
 	pub fn lua_set_anchor(lua: &Lua, (x, y): (Option<usize>, Option<usize>))
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		let cur_pos = editor.buffer.cursor_pos();
 		let anchor_x = x.unwrap_or(cur_pos.x);
@@ -180,27 +183,27 @@ pub mod api
 	pub fn lua_clear_anchor(lua: &Lua, _: ())
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 
 		editor.clear_anchor();
 		
 		return Ok(());
 	}
 	
-	pub fn lua_delete_selected(lua: &Lua)
+	pub fn lua_delete_selected(lua: &Lua, _: ())
 	-> mlua::Result<()>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		editor.delete_selected();
 		
 		return Ok(());
 	}
 	
-	pub fn lua_get_cursor_pos(lua: &Lua)
+	pub fn lua_get_cursor_pos(lua: &Lua, _: ())
 	-> mlua::Result<(usize, usize)>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		let cur_pos = editor.buffer.cursor_pos();
 		
@@ -210,7 +213,7 @@ pub mod api
 	pub fn lua_get_line_end(lua: &Lua, line: Option<usize>)
 	-> mlua::Result<(usize)>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		let target = line.unwrap_or(editor.buffer.cursor_pos().y);
 		let line_end = editor.buffer.get_line_length(target);
@@ -218,11 +221,77 @@ pub mod api
 		return Ok((line_end));
 	}
 	
+	pub fn lua_char_at(lua: &Lua, (x, y): (Option<usize>, Option<usize>))
+	-> mlua::Result<Option<u8>>
+	{
+		let editor = unsafe {get_editor(lua)?};
+		let cur_pos = editor.buffer.cursor_pos();
+		let x = x.unwrap_or(cur_pos.x);
+		let y = y.unwrap_or(cur_pos.y);
+		
+		let abs_pos = editor.buffer.repos_to_abspos(Position{ x, y });
+		
+		return Ok(editor.buffer.char_at(abs_pos));
+	}
+	
+	pub fn lua_get_max_line_index(lua: &Lua, _: ())
+	-> mlua::Result<usize>
+	{
+		let editor = unsafe {get_editor(lua)?};
+		return Ok(editor.buffer.max_line_index());
+	}
+	
+	pub fn lua_get_current_mode(lua: &Lua, _: ())
+	-> mlua::Result<Option<String>>
+	{
+		let editor = unsafe {get_editor(lua)?};
+		return Ok(editor.mode_info.cur_mode.clone());
+	}
+	
+	pub fn lua_get_saved_mode(lua: &Lua, _:())
+	-> mlua::Result<Option<String>>
+	{
+		let editor = unsafe {get_editor(lua)?};
+		return Ok(editor.mode_info.prev_mode.clone());
+	}
+	
+	pub fn lua_restore_mode(lua: &Lua, _: ())
+	-> mlua::Result<()>
+	{
+		let editor = unsafe {get_editor(lua)?};
+		editor.restore_mode();
+		return Ok(());
+	}
+	
+	pub fn lua_call_mode_hook(lua: &Lua, (mode_name, hook_name): (String, String))
+	-> mlua::Result<()>
+	{
+		let editor = unsafe {get_editor(lua)?};
+		editor.call_mode_hook(&mode_name, &hook_name);
+		return Ok(());
+	}
+	
+	pub fn lua_delete_after(lua: &Lua, _: ())
+	-> mlua::Result<()>
+	{
+		let editor = unsafe {get_editor(lua)?};
+		editor.buffer.delete_after();
+		return Ok(());
+	}
+	
+	pub fn lua_delete_before(lua: &Lua, _: ())
+	-> mlua::Result<()>
+	{
+		let editor = unsafe {get_editor(lua)?};
+		editor.buffer.delete_before();
+		return Ok(());
+	}
+	
 	pub fn lua_forward_match(lua: &Lua,
 		(matcher, from_x, from_y): (u8, Option<usize>, Option<usize>))
 	-> mlua::Result<mlua::MultiValue>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		let cur_pos = editor.buffer.cursor_pos();
 		let from_x = from_x.unwrap_or(cur_pos.x);
@@ -256,7 +325,7 @@ pub mod api
 		(matcher, from_x, from_y): (u8, Option<usize>, Option<usize>))
 	-> mlua::Result<mlua::MultiValue>
 	{
-		let editor = get_editor(lua)?;
+		let editor = unsafe {get_editor(lua)?};
 		
 		let cur_pos = editor.buffer.cursor_pos();
 		let from_x = from_x.unwrap_or(cur_pos.x);
